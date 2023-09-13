a26c77314a054834ca01ad0a03bb3ff04835d4c7
Guilherme S <guinho.shuto@gmail.com>
2023-09-12 23:20:14 -0400

    começo do front

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { Button } from "./components/ui/button"
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
+
 export function App() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1>upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido com ❤️</span>
+          <Separator orientation="vertical" className="h-6"/>
+          <Button variant="outline"><Github className="w-4 h-4 mr-2"/>Github</Button>
+        </div>
+      </div>
+      {/* main */}
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea />
+            <Textarea />
+          </div>
+          <p className="text-sm text-muted-foreground">Lembre-se: você pode usar a variável <code className="text-violet-400">{'{'}transcription{'}'}</code> no seu prompt para adicionar o conteúdo da transcrição no vídeo selecionado</p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
+    </div>
   )
 }
 
